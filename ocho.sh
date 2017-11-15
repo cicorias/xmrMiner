0cac309c7c3c32235368d9be34444e699f9d9441
Dave Puchyr <dave@ocho.avaritia.com>
2017-11-15 21:48:44 +0100

    Dropped debug args.

--- ocho.sh
+++ ocho.sh
@@ -7,5 +7,5 @@ else
    ( cd build && rm -f CMakeCache.txt && cmake -DCUDA_ARCH=20 -DCMAKE_BUILD_TYPE=Release .. && make -j6 );
 fi
 
-./build/xmrMiner --debug --debugDev --donate=0 --launch=32x16 --bfactor=10 --bsleep=10 --url=stratum+tcp://xmrpool.eu:5555 --keepalive --pass=x --user=45qYEXVGwo12ZaKHzDSB4f7ktjMysjddFCffQLrjs2yT5U37zHgYpPNWdAKc9QKLXST1zyXrouNJTQxeYP1JT1yhUjQbor6+ocho
+./build/xmrMiner --donate=0 --launch=32x16 --bfactor=10 --bsleep=10 --url=stratum+tcp://xmrpool.eu:5555 --keepalive --pass=x --user=45qYEXVGwo12ZaKHzDSB4f7ktjMysjddFCffQLrjs2yT5U37zHgYpPNWdAKc9QKLXST1zyXrouNJTQxeYP1JT1yhUjQbor6+ocho
 
